71e91ce4da6554a1d169155d0bab145acb2432ae
Nikita Karmakar <karmakarnikita412@gmail.com>
2025-03-20 19:49:06 +0530

    Update blogController.js

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,1 +1,31 @@
+const Blog = require('../models/Blog');
+const { Configuration, OpenAIApi } = require('openai');
 
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+exports.generateBlog = async (req, res) => {
+  const { prompt } = req.body;
+  try {
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `Write a blog on: ${prompt}`,
+      max_tokens: 800,
+    });
+    const blog = new Blog({
+      title: prompt,
+      content: response.data.choices[0].text,
+    });
+    await blog.save();
+    res.status(201).json(blog);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getBlogs = async (req, res) => {
+  const blogs = await Blog.find().sort({ createdAt: -1 });
+  res.json(blogs);
+};
